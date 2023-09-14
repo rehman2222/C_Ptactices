933ea2133bdba23a5658114f3e93fdb2dfcfe1ee
Abdur Rehman <144209289+rehman2222@users.noreply.github.com>
2023-09-13 21:59:24 -0700

    Upload Two C programs

---
+++ increeee.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+main()
+{
+	int x=3,y;
+//	y=x++;
+	//printf("%d \n%d ",x,y); 
+	y=++x;
+	printf("%d \n%d",x,y);
+	
+	
+	
+	
+	
+	
+	return 0;
+}
---
+++ sum.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+main()
+{
+	int x,y;
+	printf("Enter two numbers \n");
+	scanf("%d%d",&x,&y);
+	printf("sum of %d and %d is %d",x,y,x+y);
+	
+	
+	
+	
+	
+	
+	return 0;
+}
